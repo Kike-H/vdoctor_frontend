dfb6d9c102d7130558cc213703b62643236e9fe2
Manuel E. Hernandez <kicke10h@gmail.com>
2022-02-09 02:26:36 -0600

    Firts Commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import 'package:vdoctor_frontend/screens/login.dart';
+import 'package:vdoctor_frontend/screens/signup.dart';
+
+void main() {
+	runApp(const MaterialApp(
+		debugShowCheckedModeBanner: true,
+		home: HomePage(),
+	));
+}
+
+class HomePage extends StatelessWidget {
+
+	const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          // we will give media query height
+          // double.infinity make it big as my parent allows
+          // while MediaQuery make it big as per the screen
+
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Stack(
+                children:  <Widget>[
+									Container(
+										child: Stack(
+											children: <Widget>[
+												Container(
+													padding: const EdgeInsets.fromLTRB(15.0, 10.0, 0.0, 0.0),
+													child: const Text(
+														'vDoctor',
+														style: TextStyle(
+															fontFamily: 'Montserrat',
+															fontSize: 70, 
+															fontWeight: FontWeight.bold
+														)
+													),
+												)
+											],
+										),
+									),
+									Container(
+										padding: const EdgeInsets.fromLTRB(15.0, 75.0, 0.0, 0.0),
+										child: const Text(
+											'Be Safe',
+											style: TextStyle(
+												fontSize: 50,
+												fontWeight: FontWeight.bold
+											),
+										),
+									),
+									Container(
+										padding: const EdgeInsets.fromLTRB(200.0, 45.0, 0.0, 0.0),
+										child: const Text(
+											'.',
+											style: TextStyle(
+												fontSize: 80,
+												fontWeight: FontWeight.bold,
+												color: Colors.blue
+											),
+										),
+									),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/img/welcome.png")
+                  )
+                ),
+              ),
+
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                  const SizedBox(height:20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+                    },
+                    color: const Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
