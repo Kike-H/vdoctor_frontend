db596196d03f3bd83bfd7292504e5a3c8d475737
Manuel E. Hernandez <kicke10h@gmail.com>
2022-03-21 15:15:05 -0600

    Remove unsused rooth

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
 import 'package:vdoctor_frontend/screens/login.dart';
 import 'package:vdoctor_frontend/screens/signup.dart';
 
 void main() {
-	runApp(const MaterialApp(
-		debugShowCheckedModeBanner: true,
-		home: HomePage(),
-	));
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: true,
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatelessWidget {
-	const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,45 +30,36 @@ class HomePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Stack(
-                children:  <Widget>[
-									Container(
-										child: Stack(
-											children: <Widget>[
-												Container(
-													padding: const EdgeInsets.fromLTRB(15.0, 10.0, 0.0, 0.0),
-													child: const Text(
-														'vDoctor',
-														style: TextStyle(
-															fontFamily: 'Montserrat',
-															fontSize: 70, 
-															fontWeight: FontWeight.bold
-														)
-													),
-												)
-											],
-										),
-									),
-									Container(
-										padding: const EdgeInsets.fromLTRB(15.0, 75.0, 0.0, 0.0),
-										child: const Text(
-											'Be Safe',
-											style: TextStyle(
-												fontSize: 50,
-												fontWeight: FontWeight.bold
-											),
-										),
-									),
-									Container(
-										padding: const EdgeInsets.fromLTRB(200.0, 45.0, 0.0, 0.0),
-										child: const Text(
-											'.',
-											style: TextStyle(
-												fontSize: 80,
-												fontWeight: FontWeight.bold,
-												color: Colors.blue
-											),
-										),
-									),
+                children: <Widget>[
+                  Container(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(15.0, 10.0, 0.0, 0.0),
+                          child: const Text('vDoctor',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 70,
+                                  fontWeight: FontWeight.bold)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(15.0, 75.0, 0.0, 0.0),
+                    child: const Text(
+                      'Be Safe',
+                      style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(200.0, 45.0, 0.0, 0.0),
+                    child: const Text(
+                      '.',
+                      style:
+                          TextStyle(fontSize: 80, fontWeight: FontWeight.bold, color: Colors.blue),
+                    ),
+                  ),
                   const SizedBox(
                     height: 5,
                   ),
@@ -78,10 +68,7 @@ class HomePage extends StatelessWidget {
               Container(
                 height: MediaQuery.of(context).size.height / 3,
                 decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/img/welcome.png")
-                  )
-                ),
+                    image: DecorationImage(image: AssetImage("assets/img/welcome.png"))),
               ),
               Column(
                 children: <Widget>[
@@ -94,38 +81,28 @@ class HomePage extends StatelessWidget {
                     },
                     // defining the shape
                     shape: RoundedRectangleBorder(
-                      side: const BorderSide(
-                        color: Colors.black
-                      ),
-                      borderRadius: BorderRadius.circular(50)
-                    ),
+                        side: const BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(50)),
                     child: const Text(
                       "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
                     ),
                   ),
                   // creating the signup button
-                  const SizedBox(height:20),
+                  const SizedBox(height: 20),
                   MaterialButton(
                     minWidth: double.infinity,
                     height: 60,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+                    onPressed: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => SignupPage()));
                     },
                     color: const Color(0xff0095FF),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)
-                    ),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
                     child: const Text(
                       "Sign up",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
+                      style:
+                          TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18),
                     ),
                   )
                 ],
